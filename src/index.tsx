9dbb786796ae37f0b0f58c35ef4f009b09372365
Matheus Dias <dias.math@gmail.com>
2023-02-03 21:31:58 -0300

    feat: added routes component in the main file and added some new variables in the style

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './styles.sass';
-//import Login from './presentation/pages/login';
-import Header from './presentation/components/header';
-import Navigation from './presentation/components/navigation';
+import AppRouter from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Header />
-    <Navigation />
-    <main>{/* <Login /> */}</main>
+    <AppRouter />
   </React.StrictMode>
 );
